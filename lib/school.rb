ee8107b9f0e597998e43c15f4e1a048441df3fe4
Jack Quinoa <jquinoa208@gmail.com>
2020-05-25 21:13:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,7 +8,9 @@ class School
   end
   
   def add_student(student_name, student_grade) 
-    @roster[student_grade] = []
+    if @roster[grade] nil?
+      @roster[student_grade] = []
+    end
     @roster[student_grade] << student_name
     @roster << {student_grade=>[student_name]}
   end
