39341e59c50f63014333e6eae16e7a50a69d4cb0
Jack Quinoa <jquinoa208@gmail.com>
2020-05-25 21:07:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School 
+  
+  attr_accessor :roster
+  
+  def initialize(school_name)
+    @school_name = school_name 
+    @roster = {}
+  end
+  
+  def add_student(student_name,student_grade) 
+   @student_name = student_name
+    @student_grade = student_grade
+    @roster[@student_grade] = []
+    @roster[@student_grade] << @student_name
+    @roster << {@student_grade=>[@student_name]}
+  end
+end
